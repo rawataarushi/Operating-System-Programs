5f397135cf514c158cdadda2e224c9792d5708ba
Aarushi Rawat <aarushirawat43@gmail.com>
2025-12-11 19:16:56 +0530

    Added cscan.cpp

--- practice/cscan.cpp
+++ practice/cscan.cpp
@@ -1,53 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int size = 8;
+int size = 6;
 int disk_size = 200;
 
-void CSCAN(int arr[], int head)
+void CSCAN(int arr[], int head,int distance)
 {
     int seek_count=0;
     vector<int>left,right;
-    vector<int>seek_sequence;
+    vector<int>sequence;
     left.push_back(0);
     right.push_back(199);
     for(int i=0;i<size;i++)
     {
-        if(arr[i]<head) left.push_back(arr[i]);
-        if(arr[i]>head) right.push_back(arr[i]);
+        if(arr[i]<head)left.push_back(arr[i]);
+        else right.push_back(arr[i]);
     }
-    sort(left.begin(),left.end());
-    sort(right.begin(),right.end());
-    int curr;
-    for(int i=0;i<right.size();i++)
+    if(distance==1)
     {
-        curr=right[i];
-        seek_sequence.push_back(curr);
-        seek_count+=(curr-head);
-        head=curr;
+        sort(left.begin(),left.end());
+        sort(right.begin(),right.end());
+        int curr;
+        for(int i=0;i<right.size();i++)
+        {
+            curr=right[i];
+            seek_count+=(curr-head);
+            sequence.push_back(right[i]);
+            head=curr;
+        }
+        head=0;
+        seek_count+=199;
+        for(int i=0;i<left.size();i++)
+        {
+            curr=left[i];
+            seek_count+=(curr-head);
+            sequence.push_back(left[i]);
+            head=curr;
+        }
+        cout<<"total seek movement"<<seek_count<<endl;
+        for(int i : sequence)cout<<i<<" ";
     }
-    head=0;
+    else{
+        sort(left.begin(),left.end(),greater<int>());
+        sort(right.begin(),right.end(),greater<int>());
+        int curr;
+        for(int i=0;i<left.size();i++)
+        {
+            curr=left[i];
+            seek_count+=(head-curr);
+            sequence.push_back(curr);
+            head=curr;
+        }
+        head=199;
+        seek_count+=(199);
+        for(int i=0;i<right.size();i++)
+        {
+            curr=right[i];
+            seek_count+=(head-curr);
+            sequence.push_back(curr);
+            head=curr;
+        }
+        cout<<"the total seek count"<<seek_count<<endl;
+        for(int i:sequence)cout<<i<<" ";
 
-    seek_count+=(disk_size-1);
-     for(int i=0;i<left.size();i++)
-    {
-        curr=left[i];
-        seek_sequence.push_back(curr);
-        seek_count+=(curr-head);
-        head=curr;
     }
-    cout<<"the total distance is"<<seek_count<<endl;
-    cout<<"the seek sequence is :"<<endl;
-    for(int i : seek_sequence)cout<<i<<" ";
 }
 
 int main()
 {
-    int arr[size] = { 176, 79, 34, 60, 92, 11, 41, 114 };
-    int head = 50;
+    int arr[size] = { 23,89,132,42,187,14 };
+    int head = 100;
 
     cout << "Initial position of head: " << head << endl;
-    CSCAN(arr, head);
+    int distance;
+    cout<<"1 for right , 0 for left"<<endl;
+    cin>>distance;
+    CSCAN(arr, head,distance);
 
     return 0;
 }
